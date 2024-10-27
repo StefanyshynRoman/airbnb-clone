1009a8a5046ebde46d5ffc55be9e5ce9f7159586
roman <romanpz051@gmail.com>
2024-10-27 19:00:57 +0100

    0.0.1.0-layout

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,55 @@
-import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBan,
+  faBars,
+  faBox,
+  faChampagneGlasses,
+  faChessRook,
+  faCircle,
+  faCircleNotch,
+  faDollarSign,
+  faEarthEurope,
+  faExclamation,
+  faEye,
+  faFire,
+  faHillRockslide, faHouse, faKitchenSet, faLightbulb, faMinus, faMugSaucer, faPersonSkiing, faPlus, faSailboat,
+  faSearch, faSnowflake, faTent, faTowerObservation, faTractor, faTrashCan, faTree, faUmbrellaBeach,
+  faUser, faWater,
+  IconDefinition
+} from "@fortawesome/free-solid-svg-icons";
 import {faAirbnb} from '@fortawesome/free-brands-svg-icons';
 
-export const fontAwesomeIcons:IconDefinition[]=[
+export let fontAwesomeIcons: IconDefinition[];
+fontAwesomeIcons = [
+  faBan,
+  faBars,
+  faBox,
+  faChampagneGlasses,
+  faChessRook,
+  faCircle,
+  faCircleNotch,
+  faDollarSign,
+  faEarthEurope,
+  faExclamation,
+  faEye,
+  faFire,
+  faHillRockslide,
+  faHouse,
+  faKitchenSet,
+  faLightbulb,
+  faMinus,
+  faMugSaucer,
+  faPersonSkiing,
+  faPlus,
+  faSailboat,
+  faSearch,
+  faSnowflake,
+  faTent,
+  faTowerObservation,
+  faTractor,
+  faTrashCan,
+  faTree,
+  faUmbrellaBeach,
+  faUser,
+  faWater,
   faAirbnb
 ]
